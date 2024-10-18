6a5df40258c7a39a3a78f90c9d31a10ab312311b
srihari985 <123962312+srihari985@users.noreply.github.com>
2024-10-18 14:40:57 +0530

    Delete ERMS-Application-17-10-24-depoly directory

--- ERMS-Application-17-10-24-depoly/src/main/java/com/erms/ERMS_Application/Demo/video/DemoVideoRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.erms.ERMS_Application.Demo.video;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface DemoVideoRepository extends JpaRepository<DemoVideo,Long> {
-//
-//}
--- ERMS-Application-17-10-24-depoly/src/main/java/com/erms/ERMS_Application/Quotation/AddParty/AddPartyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.erms.ERMS_Application.Quotation.AddParty;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddPartyRepo extends JpaRepository<AddPartyEntity, Long>{
-
-}
