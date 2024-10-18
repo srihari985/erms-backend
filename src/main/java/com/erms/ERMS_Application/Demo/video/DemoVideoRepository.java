1ebf215aa188bed8e47d6f5bf10e33f684ae213e
Your Name <you@example.com>
2024-10-18 09:19:59 +0000

    map

---
+++ src/main/java/com/erms/ERMS_Application/Demo/video/DemoVideoRepository.java
@@ -0,0 +1,7 @@
+//package com.erms.ERMS_Application.Demo.video;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface DemoVideoRepository extends JpaRepository<DemoVideo,Long> {
+//
+//}
